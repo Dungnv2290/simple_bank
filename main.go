ce29961d60f11b388b1cee6211aca31073e22faa
Minh <117076980+MinhBnK@users.noreply.github.com>
2024-01-14 11:08:31 +0700

    Add validation for gRPC request

--- main.go
+++ main.go
@@ -9,11 +9,13 @@ import (
 
 	"github.com/dungnguyen/simplebank/api"
 	db "github.com/dungnguyen/simplebank/db/sqlc"
+	_ "github.com/dungnguyen/simplebank/doc/statik"
 	"github.com/dungnguyen/simplebank/gapi"
 	"github.com/dungnguyen/simplebank/pb"
 	"github.com/dungnguyen/simplebank/util"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	_ "github.com/lib/pq"
+	"github.com/rakyll/statik/fs"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 	"google.golang.org/protobuf/encoding/protojson"
@@ -63,6 +65,14 @@ func runGatewayServer(config util.Config, store db.Store) {
 
 	mux := http.NewServeMux()
 	mux.Handle("/", grpcMux)
+
+	statikFS, err := fs.New()
+	if err != nil {
+		log.Fatal("cannot create statik fs:", err)
+	}
+
+	swaggerHandler := http.StripPrefix("/swagger/", http.FileServer(statikFS))
+	mux.Handle("/swagger/", swaggerHandler)
 
 	listener, err := net.Listen("tcp", config.HTTPServerAddress)
 	if err != nil {
